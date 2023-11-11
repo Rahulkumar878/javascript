663894c96ac939d47c2c16554fbca91101597813
Rahulkumar878 <78690062+Rahulkumar878@users.noreply.github.com>
2023-11-11 14:16:28 +0000

    datatype.js

---
+++ datatype.js
@@ -0,0 +1,20 @@
+"use strict";
+// alert(3+3);we are using node js not browser
+let name ="rahul";
+let age=18;
+let isLoggedIn=false;
+//number 2 to power 53
+//bignt
+BigInt=121435;
+//INT
+//BIGINT
+//BOOLEAN
+//STRING
+//NULL
+//UNDEFINED
+//symbol =>> unique
+
+
+//object
+console.log(typeof("age"));
+
