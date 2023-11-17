d89473efaf0656624a275ea072ac9a8de71e5fb4
Rahulkumar878 <78690062+Rahulkumar878@users.noreply.github.com>
2023-11-17 08:22:24 +0000

    comparision.js

---
+++ javascript/comparison.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2!=1);
+// console.log("2">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(undefined==0);
+// console.log("2"===2);
+//primitive datatype - number,bigInt,symbol,undefined,null,boolean,string
+//nonprimitive datatype - array,object,function
+// const id1=Symbol('123');
+// const id2=Symbol('123');
+// console.log(id1," ",id2);
+// console.log(id1==id2);
+const hero=["shaktiman","nagaargun"];
+let obj={
+    name:"rahul",
+    age:22,
+}
+const myfunction=function()
+{
+    console.log("hiii rahul");
+}
+console.log(hero);
+console.log(obj);
+console.log(typeof(myfunction));
+console.log(typeof(null));
