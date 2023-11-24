02bbd094dbc7bae2728c0ea6aaf5443ae1a35ec7
Rahulkumar878 <78690062+Rahulkumar878@users.noreply.github.com>
2023-11-24 15:26:59 +0000

    number.js

---
+++ javascript/string.js
@@ -0,0 +1,18 @@
+// "hello"+"world"
+// 'hello'+'world'
+const name ="rahul";
+const repocount=50;
+console.log(name+repocount+"value");
+console.log(`hello my name is ${name} and my repo count is${repocount}`);
+ const gamename=new String("rahul Kumar");
+ console.log(gamename);
+ console.log(gamename.__proto__);
+ console.log(gamename.length);
+ console.log(gamename.toUpperCase());
+ console.log(gamename.charAt(3));
+ console.log(gamename.indexOf('u'));
+ console.log(gamename.substring(0,4)); 
+ const val=new String("minshu");
+ console.log(val.slice(-6,3));
+ const newstring="     Rahul Kumar     ";
+ console.log(newstring.trim);
