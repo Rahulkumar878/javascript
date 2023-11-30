4b20a6ef6b38152b4c65ceceabb8b897a4312afd
Rahulkumar878 <78690062+Rahulkumar878@users.noreply.github.com>
2023-11-30 08:57:50 +0000

    json.js

---
+++ javascript/json.js
@@ -0,0 +1,16 @@
+// const course={
+//     coursename:"js ins hindi",
+//     price:"999",
+//     courseInstructor:"rahul"
+// }
+// const {courseInstructor:instructor}=course;
+// console.log(instructor);
+// {
+//    " name":"rahul",
+//     "course":"js",
+//     "price":"free"
+// }
+[
+    {},{},
+    {}
+]
